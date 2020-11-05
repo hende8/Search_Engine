df822ab015198896f09eb1b3904736a91087f0fb
hende <hendebii@gmail.com>
2020-11-05 17:51:51 +0200

    update ReadFile (read_all_files function) read within directories of corpus

--- reader.py
+++ reader.py
@@ -15,4 +15,17 @@ class ReadFile:
         """
         full_path = os.path.join(self.corpus_path, file_name)
         df = pd.read_parquet(full_path, engine="pyarrow")
+        print(df)
         return df.values.tolist()
+
+    def read_all_files(self):
+        for filename in os.listdir(self.corpus_path):
+            folder = self.corpus_path +'\\'+filename
+            if os.path.isdir(self.corpus_path +'\\'+filename):
+                for filenameParquet in os.listdir(folder):
+                    if  filenameParquet.endswith(".parquet"):
+                        folder=folder+'\\'+filenameParquet
+                        self.read_file(folder)
+                        break
+            else:
+                continue

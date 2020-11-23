df3f88221ade64a5c5ee9e1923dff11fd233f22b
niv2301 <nivosmeir@gmail.com>
2020-11-23 04:18:43 +0200

    union parser with indexer and posting file. parser did 3.386M docs in 2693 sec

--- parser_module.py
+++ parser_module.py
@@ -16,8 +16,9 @@ class Parse:
         self.dic_entities = {}
         self.dic_entities_index = {}
         self.array_names_and_entities = {}
-        self.covid_list = ["corona", "covid", "covid-19", "covid19", "coronavirus", "coronaviruses", "#covid19", "#corona",
-                      "#coronaviruses", "#covid", "#covid-19", "#coronavirus"]
+        self.covid_list = ["corona", "covid", "covid-19", "covid19", "coronavirus", "coronaviruses", "#covid19",
+                           "#corona", "#COVIDー19", "#coronaviruses", "#covid", "#covid-19", "#coronavirus"]
+        self.list_percent = ["percent", "Percent", "Percentage", "percentage"]
 
     def parse_sentence(self, text, stemmer=False):
         """
@@ -27,72 +28,64 @@ class Parse:
         """
         # text= re.sub(r'http\S+|www.\S+', '', 'www.ynet.co.il')
         # print(text)
-        # text = "##stay_At_Home"
-        text = "https:// 100⁰ 3.63 million say “not at all.” I walked in corona the Corona in Corona  covid-19 streed. COVID-19 and he found 10 million dollar for 6 percent"
-        # text = "Meet walked Donald Trump in Y'all Tom the streed and he found 10 million dollar for 6 percent https://www.rawstory.com/2020/07/trump-to-blow-off-cdc-recommendations-and-issue-his-own-guidelines-for-reopening-schools-report"
+        # text = "6 9 0 7a ❶ ³"
+
+        # text = "100⁰ # \ | @ ! $ % ^ & * ( ) ` ~ + = _ \ ' | : ; / ? . , } { ] [ https://  3.63 million say “not at all.” I walked in corona the Corona in Corona  covid-19 streed. COVID-19 and he found 10 million dollar for 6 percent"
+        #text = "€£💐🔃🙏🏻❤⬇🐮💗，🎊🎁🎉🎈🍸‘🤦🏻‍♀🕯🙏🏼🔹€4️⃣🐣👍🏼🍰🚨💥🇺🇸🚫✅❗️👇⁦👏⁩“”⚠🇦🇷‼😭😩😪🤬🤡😷😁😳😂😢🤣⑥²⁸¹❶❷❽②⑦&$.,!?,…:;^ Meet walked Donald Trump in Y'all Tom the streed and he found 10 million dollar for 6 percent https://www.rawstory.com/2020/07/trump-to-blow-off-cdc-recommendations-and-issue-his-own-guidelines-for-reopening-schools-report"
         self.array_names_and_entities = {}
         text = text.replace("\n", ". ")
+        text = self.ignore_emojis(text)
         array_text_space = text.split(" ")
         array_text_space = self.seperate_words_with_dots(array_text_space)
-        # print(text)
         string_ans = ""
         array_size = range(len(array_text_space))
         string_ans_index = 0
         entities_url = []  # help us to replace the url to "" because in get_entities it returns parts of the url
         for word, idx in zip(array_text_space, array_size):
             ans = ""
+            # print(text)
+            if word == '' or word == ' ': continue
+            check_digit = self.isdigit(word)
+            if (len(word) < 2 and check_digit is False) or self.is_ascii(word) is False:
+                word = self.remove_panctuation(word)
+                if self.is_ascii(word) is False or word == '' or word == " ":
+                    # print(word)
+                    continue
             if ans == "" and self.is_url(word):
                 entities_url.append(word)
                 if "t.co" in word: continue
                 ans = self.parse_url(word)
+                if ans == "": entities_url.remove(word)
             else:
-                if ans == "" and len(word) > 1 and word[0] != '#' and self.is_ascii(word) and not self.isfloat(word):
+                if ans == "" and len(word) < 2 and word[0] != '#' and self.is_ascii(word) and not self.isfloat(word):
                     word = self.remove_panctuation(word)
-                elif ans == "" and word == "" or (len(word) == 1 and word[0] == '#') or word == "¹⁹":
-                    continue
-            if ans == "" and len(word) > 1 and word[0] == '#':
+            if ans == "" and word[0] == '#':
                 temp_word = self.remove_panctuation(word)
                 if temp_word == "" or temp_word == "#":
                     continue
                 ans = self.parse_hashtag(temp_word)
-            elif ans == "" and len(word) == 1 and (word[0] == '#' or word[0] == '³'):
-                continue
-            elif ans == "" and len(word) > 1 and word[0] == '@':
-                ans = word
-            elif ans == "" and "percent" == word or "Percent" == word or "Percentage" == word or "percentage" == word:
+            elif ans == "" and word[0] == '@':
+                ans = self.remove_panctuation(word)
+            elif ans == "" and word in self.list_percent:
                 if idx > 0 and self.isfloat(array_text_space[idx - 1]):
                     ans = self.parse_percentage(array_text_space[idx - 1] + " " + word)
                     string_ans = string_ans[:len(string_ans) - 1 - len(ans)] + string_ans[
                                                                                len(string_ans) + len(word):] + " "
                 else:
                     ans = word
-            elif ans == "" and word.lstrip('-').isdigit() or self.isfloat(word.lstrip('-')) or self.isFraction(
-                    word.lstrip('-')):
+            elif ans == "" and (
+                    word.lstrip('-').isdigit() or self.isfloat(word.lstrip('-')) or self.isFraction(word.lstrip('-'))):
                 ans = self.convert_str_to_number(array_text_space, idx)
-
-            if len(word) > 1 and ans == "":
+            if ans == "":
                 if word.lower() in self.stop_words: continue
-                ans = word
-            if ans != "":
-                string_ans += self.add_to_dictionary(ans, string_ans_index)
-                string_ans_index += len(word) + 1
+                ans = self.remove_panctuation(word)
+            #ans = word
+            string_ans += self.add_to_dictionary(ans, string_ans_index)
+            string_ans_index += len(word) + 1
 
         array_names_and_entities = self.get_name_and_entities(entities_url, array_text_space)
         array_parsed = string_ans.split()
         return array_parsed
-        # array_parsed_stemmer = []
-        # stemmer_s = PorterStemmer()
-        # temp_array = []
-        # if stemmer:
-        #     for term in array_parsed:
-        #         if term[0].isupper():
-        #             temp_array.append(term)
-        #         else:
-        #             temp_array.append(stemmer_s.stem(term))
-        #     array_parsed = temp_array
-        # # for term in entities_and_names:
-        # #     array_parsed.append(term)
-        # return array_parsed
 
     def seperate_words_with_dots(self, array_text):
         new_text = ""
@@ -125,13 +118,6 @@ class Parse:
             r'(?::\d+)?'  # optional port
             r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-        # regex = re.compile(
-        #     r'^(?:http|ftp)s?://'  # http:// or https://
-        #     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        #     r'localhost|'  # localhost...
-        #     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        #     r'(?::\d+)?'  # optional port
-        #     r'(?:/?|[/?]\S+)$', re.IGNORECASE)
         return re.match(regex, text) is not None
 
     def add_to_dictionary(self, text, index):
@@ -173,10 +159,6 @@ class Parse:
             if ans_len > 0:
                 remove_www = ans[0].replace("www.", "")
                 ans[0] = ans[0].replace(ans[0], remove_www)
-            # ans = " ".join(ans).strip()
-            # ans = remove_www + " " + ans
-            # ans = re.sub(r'http|\swww.+','',ans) ## not work well
-            # array_url = ans.split()
                 string_without_stopword = ""
                 length = range(len(ans))
                 for word, idx in zip(ans, length):
@@ -186,6 +168,11 @@ class Parse:
                 return string_without_stopword.lstrip()
             else:
                 return ""
+
+    def isdigit(self, word):
+        if "0" <= word <= "9":
+            return True
+        return False
 
     def isfloat(self, value):
         """
@@ -255,13 +242,13 @@ class Parse:
                 text_return += 'K'
                 text_demo[idx + 1] = ""
             return help_minus + ''.join(text_return)
-        if not math.isnan(float(my_word)):
+        if my_word != '' and not math.isnan(float(my_word)):
             number = float(my_word)
             number_numerize = self.convert_str_to_number_kmb(number)
             if idx + 1 < len(text_demo):
                 token_next = text_demo[idx + 1].lower()
                 number_to_input = str(number_numerize)
-                if token_next.__eq__("billion") or token_next.__eq__("billions"):
+                if token_next == "billion" or token_next == "billions":
                     if 'K' in number_numerize or 'M' in number_numerize:
                         number_to_input = (number_to_input.translate({ord('K'): None}))
                         number_to_input = (number_to_input.translate({ord('M'): None}))
@@ -270,7 +257,7 @@ class Parse:
                         text_return.append(str(number_numerize + 'B'))
                     text_demo[idx + 1] = ""
 
-                elif token_next.__eq__("million") or token_next.__eq__("millions"):
+                elif token_next == "million" or token_next == "millions":
                     if 'K' in number_numerize:
                         number_to_input = (number_to_input.translate({ord('K'): None}))
                         text_return.append(number_to_input + 'B')
@@ -278,7 +265,7 @@ class Parse:
                         number_to_input = str(number_numerize)
                         text_return.append(number_to_input + 'M')
                     text_demo[idx + 1] = ""
-                elif token_next.__eq__("thousand") or token_next.__eq__("thousands"):
+                elif token_next == "thousand" or token_next == "thousands":
                     if 'K' in number_numerize:
                         number_to_input = (number_to_input.translate({ord('K'): None}))
                         text_return.append(number_to_input + 'M')
@@ -298,9 +285,33 @@ class Parse:
                 text_return.append(text_demo[idx])
         return help_minus + ' '.join(text_return)
 
+    def ignore_emojis(self, text):
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002500-\U00002BEF"  # chinese char
+                                   u"\U00002702-\U000027B0"
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u"\U00010000-\U0010ffff"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u200d"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\ufe0f"  # dingbats
+                                   u"\u3030"
+                                   "]+", flags=re.UNICODE)
+        ans = emoji_pattern.sub(r'', text)
+        return ans
+
     def is_ascii(self, s):
-       # if s == '…' or s == '’' or s == '³' or s == "¹⁹": return True
-        return all(ord(c) < 128 or c == '…' or c == '’' or c == '³' or c == "¹⁹" for c in s)
+        ans = all(ord(c) < 128 or c == '…' or c == '’' or c == '³' or c == "¹⁹" for c in s)
+        return ans
 
     def parse_percentage(self, string):
         """
@@ -317,7 +328,7 @@ class Parse:
                 :param word
                 :return: word without panctuation
                 """
-        chars = set('.,:;!()[]{}?=+…$&')
+        # chars = set('.,:;!()[]{}?=+…$&')
         if re.match(r'[^@]+@[^@]+\.[^@]+', word): return word
         if "#" == word: return ""
         # if ('@' in word and word[0] != '@' and '.' in word):
@@ -335,7 +346,7 @@ class Parse:
         if '@' in word and word[0] != '@': word = word.replace("@", "")
 
         word = word.replace("-", " ")
-        word = re.sub(r'[&$.,!?,…:;^“"{}=+()⁰/[\[\]]', '', word)
+        word = re.sub(r'[€£€4️⃣“”‘⁦⁩‼⑥²⁸¹❶❷❽②⑦&$.,!?,…:;^"{}=+()⁰/[\[\]]', '', word)
         return word
 
     # def get_name_and_entities(self,idx, text):
@@ -369,8 +380,6 @@ class Parse:
     #     return self.array_names_and_entities
 
     def get_name_and_entities(self, entities_url, array_text_space):
-        # ttt = re.findall(r'(?:\s+[A-Z][^'\\]|\\*)*',text)
-        # test = re.findall(r'\b([A-Z][a-z[^'\\]']*)(\s[A-Z][a-z']*)*\b', text)
         text = ""
         regular_word = ""
         for corona_word in array_text_space:
@@ -388,7 +397,6 @@ class Parse:
         tokinzed_entity_new = set()
         tokinzed_entity_new.update([e for e in matches if len(e) > 1])
 
-        length = range(len(tokinzed_entity_new))
         for word in tokinzed_entity_new:
             if word.lower() not in self.stop_words:
                 all_places = [m.start() for m in re.finditer(word, text)]
@@ -482,12 +490,6 @@ class Parse:
                 term_dict[term] = 1
             else:
                 term_dict[term] += 1
-        # print(full_text)
-        # print("##########################################################")
-        # print(tokenized_text)
-        # print("##########################################################")
-        # print(self.array_names_and_entities)
-        # print("##########################################################")
         document = Document(tweet_id, tweet_date, full_text, url, retweet_text, retweet_url, quote_text,
                             quote_url, term_dict, doc_length)
         return document
